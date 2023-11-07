5fe074f38db11ab72820762f0ad1e44380d4356f
brlumar <126432304+brlumar@users.noreply.github.com>
2023-11-07 17:52:29 -0600

    added audio. may not be functional yet

--- js/questions.js
+++ js/questions.js
@@ -3,26 +3,71 @@ const cardData = [
         phrase: 'Name a fruit that is sweet',
         answers: [
             {
-                answer: 'pineapple'
+                answer: 'pineapple',
+                points: '30'
             },
             {
-                answer: 'strawberry'
+                answer: 'strawberry',
+                points: '25'
+
             },
             {
-                answer: 'orange'
+                answer: 'orange',
+                points: '20'
+
             }, 
             {
-                answer: 'grape'
+                answer: 'grape',
+                points: '15'
+
             }, 
             {
-                answer: 'mango'
+                answer: 'mango',
+                points: '10'
+
             }, 
             {
-                answer: 'apple'
+                answer: 'apple',
+                points: '5'
+
             }, 
+         
+
+        ]
+    },
+    {
+        phrase: 'When a baby is crying, what do you think is the problem?',
+        answers: [
             {
-                answer: 'banana'
+                answer: 'hungry',
+                points: '30'
             },
+            {
+                answer: 'dirty diaper',
+                points: '25'
+
+            },
+            {
+                answer: 'gas',
+                points: '20'
+
+            }, 
+            {
+                answer: 'sleepy',
+                points: '15'
+
+            }, 
+            {
+                answer: 'teething',
+                points: '10'
+
+            }, 
+            {
+                answer: 'pick me up',
+                points: '5'
+
+            }, 
+         
 
         ]
     }
--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const answerbtn3 = document.getElementById('answerbtn3');
 const answerbtn4 = document.getElementById('answerbtn4');
 const answerbtn5 = document.getElementById('answerbtn5');
 const answerbtn6 = document.getElementById('answerbtn6');
+const questionBox = document.getElementById('questionBox');
+
 
 const strikeBtn = document.getElementById('strikeBtn');
 const strike1 = document.getElementById('strike1');
@@ -11,9 +13,54 @@ const strike2 = document.getElementById('strike2');
 const strike3 = document.getElementById('strike3');
 const xbox = document.getElementById('xbox');
 
+const team1card = document.getElementById('team1card');
+const team2card = document.getElementById('team2card');
 
-let strikeTotal = 0;
+const audioCorrect = document.getElementById('correctAudio');
+const audioIncorrect = document.getElementById('incorrectAudio');
 
+
+
+let strikeTotal = 0; //keeps track of how many strikes the active team has
+let questionIndex = 0; //indicates which question we are on
+let whichTeam = 1;
+
+function removeStrikes() { //Removes strike for new turn
+    xbox.style.display = 'none';
+    strike1.style.display = 'none';
+    strike2.style.display = 'none';
+    strike3.style.display = 'none';
+    strikeTotal = 0;
+}
+
+function clearCard() {
+    questionBox.innerHTML = '';
+    answerbtn1.classList.remove('btn-light');
+    answerbtn2.classList.remove('btn-light');
+    answerbtn3.classList.remove('btn-light');
+    answerbtn4.classList.remove('btn-light');
+    answerbtn5.classList.remove('btn-light');
+    answerbtn6.classList.remove('btn-light');
+
+    answerbtn1.innerHTML = '1';
+    answerbtn2.innerHTML = '2';
+    answerbtn3.innerHTML = '3';
+    answerbtn4.innerHTML = '4';
+    answerbtn5.innerHTML = '5';
+    answerbtn6.innerHTML = '6';
+
+}
+
+function nextQuestion() {
+    removeStrikes();
+    clearCard();
+    currentQuestion = cardData[questionIndex];
+    questionBox.innerHTML = currentQuestion.phrase;
+
+
+
+    questionIndex++;
+}
 
 
 
@@ -21,35 +68,54 @@ function answer1() {
     console.log('answer1 is clicked')
 
     answerbtn1.classList.add('btn-light');
+    answerbtn1.innerHTML = currentQuestion.answers[0].answer;
+    playCorrect();
+
+
 }
 
 function answer2() {
     console.log('answer2 is clicked')
 
     answerbtn2.classList.add('btn-light');
+    answerbtn2.innerHTML = currentQuestion.answers[1].answer;
+    playCorrect();
+
 }
 
 function answer3() {
     console.log('answer3 is clicked')
 
     answerbtn3.classList.add('btn-light');
+    answerbtn3.innerHTML = currentQuestion.answers[2].answer;
+    playCorrect();
+
 }
 
 function answer4() {
     console.log('answer4 is clicked')
 
     answerbtn4.classList.add('btn-light');
+    answerbtn4.innerHTML = currentQuestion.answers[3].answer;
+    playCorrect();
+
 }
 
 function answer5() {
     console.log('answer5 is clicked')
 
     answerbtn5.classList.add('btn-light');
+    answerbtn5.innerHTML = currentQuestion.answers[4].answer;
+    playCorrect();
+
 }
 function answer6() {
     console.log('answer6 is clicked')
 
     answerbtn6.classList.add('btn-light');
+    answerbtn6.innerHTML = currentQuestion.answers[5].answer;
+    playCorrect();
+
 }
 function strikeone() {
     strike1.style.display = 'flex';
@@ -69,17 +135,43 @@ function strike() {
     if (strikeTotal == 0) {
         strike1.style.display = 'block';
         xbox.style.display = 'block';
+        playIncorrect();
 
         strikeTotal++;
-    }else if(strikeTotal==1){
+    } else if (strikeTotal == 1) {
         strike2.style.display = 'block';
         strikeTotal++;
-    }else if(strikeTotal==2){
+        playIncorrect();
+    } else if (strikeTotal == 2) {
         strike3.style.display = 'block';
         strikeTotal++;
+        playIncorrect();
     }
 }
 
+function team1active(){
+    team1card.style.backgroundColor = 'greenyellow';
+    team2card.style.backgroundColor = 'white';
+    console.log("team 1 is active");
+    whichTeam = 1;
+
+}
+
+function team2active(){
+    team1card.style.backgroundColor = 'white';
+    team2card.style.backgroundColor = 'greenyellow';
+    console.log("team 2 is active");
+    whichTeam = 2;
+}
+
+//Audio
+function playCorrect() {
+    audioCorrect.play();
+}
+
+function playIncorrect() {
+    audioIncorrect.play();
+}
 
 
 
@@ -94,4 +186,11 @@ strike1.addEventListener('click', strikeone);
 strike2.addEventListener('click', striketwo);
 strike3.addEventListener('click', strikethree);
 strikeBtn.addEventListener('click', strike);
+
+questionBox.addEventListener('click', nextQuestion);
+
+team1card.addEventListener('click', team1active);
+team2card.addEventListener('click', team2active);
+
+
 
